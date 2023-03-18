89a3e74bb51aa8bf0f0cd33fda2f393e2bf96eda
erukku <lkuida21@gmail.com>
2023-03-19 00:48:16 +0900

    add slide

---
+++ js/addslide.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+
+    var instance = M.Slider.getInstance(elems);
+  });
