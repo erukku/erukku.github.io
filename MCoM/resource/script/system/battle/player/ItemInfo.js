c0e271cf232b2b804376c81c99564b2b781c8cbd
erukku <lkuida21@gmail.com>
2025-09-09 07:18:10 +0900

    implement info of item

---
+++ MCoM/resource/script/system/battle/player/ItemInfo.js
@@ -0,0 +1,15 @@
+class ItemInfo{
+    constructor(){
+    }
+
+    getInfo(id){
+        switch(id){
+            case 0:
+                return [2,["wait",30]["heal",50]];
+
+        }
+
+    }
+}
+
+export default ItemInfo;
