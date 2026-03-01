c2e35707f6626b16b9cd24595d8cd4d90a230204
erukku <lkuida21@gmail.com>
2026-03-02 02:52:49 +0900

    setting for pages

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  // GitHub Pagesのサブディレクトリ対策（重要！）
+  base: './', 
+  build: {
+    rollupOptions: {
+      input: {
+        // メインページ
+        main: resolve(__dirname, 'index.html'),
+        // あなたが作成しているゲーム画面
+        game: resolve(__dirname, 'secoundRPG.html'), 
+      },
+    },
+  },
+});
